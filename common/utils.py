fb8d23e5f4a2ea6f938444934b143b81cb900938
bensonlin321 <bensonlin321@gmail.com>
2022-02-03 19:55:48 -0800

    fix: remove redundant code

--- common/utils.py
+++ common/utils.py
@@ -7,67 +7,6 @@ import csv
 from common.config import *
 np.set_printoptions(threshold=np.inf)
 
-def check_cache_folder():
-    file_path = "{}/cache".format(os.getcwd())
-    if(not os.path.exists(file_path)):
-        os.makedirs(file_path)
-
-def parse_multi_select(data):
-    # Not support space
-    if (' ' in data or '"' in data):
-        print("[Error] Please follow the format and enter correct input")
-        print("For example:[1,2] or 1,2")
-        os._exit(1)
-
-    try:
-        data = json.loads(data)
-    except Exception as err:
-        try:
-            data = data.split(",")
-        except Exception as err:
-            print("[Error] Please follow the format and enter correct input")
-            print("For example:[1,2] or 1,2")
-            os._exit(1)
-
-    return data
-
-
-def valid_input(data, msg):
-    try:
-        if(data == ''):
-            print("[Error] Please enter {}".format(msg))
-            return False
-        try:
-            data = int(data)
-        except ValueError:
-            print("[Error] Please enter a number")
-            return False
-        if data > 4 or data < 1:
-            print("[Error] Please use correct {}".format(msg))
-            return False
-        return True
-    except Exception as err:
-        print("[Error] Can not parse data, please check input: {}".format(err))
-        os._exit(1)
-
-def valid_single_input(data):
-    try:
-        if(data == ''):
-            print("[Error] Please enter a number")
-            return False
-        try:
-            data = int(data)
-        except ValueError:
-            print("[Error] Please enter a number")
-            return False
-        if data > 2 or data < 1:
-            print("[Error] Please use correct number")
-            return False
-        return True
-    except Exception as err:
-        print("[Error] Can not parse data, please check input: {}".format(err))
-        os._exit(1)
-
 def get_config(code_smell=FEATURE_ENVY):
     try:
         conf = Config(strategy="median")
@@ -92,17 +31,3 @@ def get_config(code_smell=FEATURE_ENVY):
     except Exception as err:
         print("[Error] Can not read config file: {}".format(err))
         os._exit(1)
-
-def load_cache(file_name):
-    try:
-        with open(file_name, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                res = {"result":(float(row['default_train']), float(row['default_train_optimized']),\
-                       float(row['default_test']), float(row['default_test_optimized']),\
-                       float(row['default_train_f1']), float(row['default_train_f1_optimized']),\
-                       float(row['default_test_f1']), float(row['default_test_f1_optimized']))}
-                return res
-    except Exception as err:
-        print("[Warning] Can not load cache: {}".format(err))
-        return {}
